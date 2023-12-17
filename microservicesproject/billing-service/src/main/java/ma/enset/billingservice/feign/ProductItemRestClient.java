a5afadef591bf1f3848ffa7ef74df28b06d8652e
salsabile18 <salsabile.massabih01@gmail.com>
2023-12-17 14:41:19 +0100

    commit

---
+++ microservicesproject/billing-service/src/main/java/ma/enset/billingservice/feign/ProductItemRestClient.java
@@ -0,0 +1,18 @@
+package ma.enset.billingservice.feign;
+
+import jakarta.ws.rs.QueryParam;
+import ma.enset.billingservice.model.Customer;
+import ma.enset.billingservice.model.Product;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.hateoas.PagedModel;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@FeignClient(name = "PRODUCT-SERVICE")
+public interface ProductItemRestClient {
+    @GetMapping(path = "/products")
+    PagedModel<Product> pageProducts();
+    @GetMapping(path = "/products/{id}")
+    Product getProductById(@PathVariable(name = "id") Long id);
+}
